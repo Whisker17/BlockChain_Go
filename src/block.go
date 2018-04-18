0c0a5276bace7ffe3947b8283e9ea972e4a584ed
Whisker17 <yukaijun123456@163.com>
2018-04-18 23:46:55 +0800

    new commit

--- src/block.go
+++ src/block.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"time"
 	"bytes"
 	"encoding/gob"
 	"log"
+	"time"
 )
 
 type Block struct {
-	Timestamp int64
-	Transactions []*Transaction
+	Timestamp     int64
+	Transactions  []*Transaction
 	PrevBlockHash []byte
-	Hash []byte
-	Nonce int
-	Height int
+	Hash          []byte
+	Nonce         int
+	Height        int
 }
 
-func NewBlock(transactions []*Transaction,prevBlockHash []byte,height int) *Block {
-	block := &Block{time.Now().Unix(),transactions,prevBlockHash,[]byte{},0,height}
+func NewBlock(transactions []*Transaction, prevBlockHash []byte, height int) *Block {
+	block := &Block{time.Now().Unix(), transactions, prevBlockHash, []byte{}, 0, height}
 	pow := NewProofOfWork(block)
-	nonce,hash := pow.Run()
+	nonce, hash := pow.Run()
 
 	block.Hash = hash[:]
 	block.Nonce = nonce
@@ -28,14 +28,14 @@ func NewBlock(transactions []*Transaction,prevBlockHash []byte,height int) *Bloc
 }
 
 func NewGenesisBlock(coinbase *Transaction) *Block {
-	return NewBlock([]*Transaction{coinbase},[]byte{},0)
+	return NewBlock([]*Transaction{coinbase}, []byte{}, 0)
 }
 
 func (b *Block) HashTransactions() []byte {
 	var transactions [][]byte
 
-	for _,tx := range b.Transactions {
-		transactions = append(transactions,tx.Serialize())
+	for _, tx := range b.Transactions {
+		transactions = append(transactions, tx.Serialize())
 	}
 	mTree := NewMerkleTree(transactions)
 
@@ -46,8 +46,8 @@ func (b *Block) Serialize() []byte {
 	var result bytes.Buffer
 	encoder := gob.NewEncoder(&result)
 
-	err := encoder.Encode()
-	if err !=nil {
+	err := encoder.Encode(b)
+	if err != nil {
 		log.Panic(err)
 	}
 
@@ -59,7 +59,7 @@ func DeserializeBlock(d []byte) *Block {
 
 	decoder := gob.NewDecoder(bytes.NewReader(d))
 	err := decoder.Decode(&block)
-	if err := nil {
+	if err != nil {
 		log.Panic(err)
 	}
 
---
+++ src/blockchain.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"github.com/boltdb/bolt"
+	"log"
+	"os"
+)
+
+const dbFile = "blockchain_%s.db"
+const blocksBucket = "blocks"
+const genesisCoinbaseData = "The Times 18/Api/2018 Chancellor on brink of second bailout for banks"
+
+type Blockchain struct {
+	tip []byte
+	db  *bolt.DB
+}
+
+func CreateBlockchain(address, nodeID string) *Blockchain {
+	dbFile := fmt.Sprintf(dbFile, nodeID)
+	if dbExists(dbFile) {
+		fmt.Println("Blockchain already exists.")
+		os.Exit(1)
+	}
+
+	var tip []byte
+
+	cbtx := NewCoinbaseTX(address, genesisCoinbaseData)
+	genesis := NewGenesisBlock(cbtx)
+
+	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucket([]byte(blocksBucket))
+		if err != nil {
+			log.Panic(err)
+		}
+
+		err = b.Put(genesis.Hash, genesis.Serialize())
+		if err != nil {
+			log.Panic(err)
+		}
+
+		err = b.Put([]byte("1"), genesis.Hash)
+		if err != nil {
+			log.Panic(err)
+		}
+		tip = genesis.Hash
+
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bc := Blockchain{tip, db}
+
+	return &bc
+}
+
+func NewBlockchain(nodeID string) *Blockchain {
+	dbFile := fmt.Sprintf(dbFile, nodeID)
+	if dbExists(dbFile) == false {
+		fmt.Println("No existing blockchain found. Create one first.")
+		os.Exit(1)
+	}
+
+	var tip []byte
+	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		tip = b.Get([]byte("1"))
+
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bc := Blockchain{tip, db}
+
+	return &bc
+}
